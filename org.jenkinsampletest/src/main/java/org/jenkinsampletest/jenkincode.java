b005732aaa920ea16b603f3a9905617d9729447d
Kalai <vijaykalaiselvan1997@gmail.com>
2024-02-20 00:40:38 +0530

    initial commit

---
+++ org.jenkinsampletest/src/main/java/org/jenkinsampletest/jenkincode.java
@@ -0,0 +1,27 @@
+package org.jenkinsampletest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class jenkincode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	 WebDriverManager.chromedriver().setup();
+	 WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.youtube.com/");
+		driver.manage().window().maximize();
+		System.out.println("Browser opened.........");
+		driver.close();
+		
+		
+
+	
+
+	}
+
+}
